393696300412541a1aa0067705b3ee03806225f3
Joshua Book <joshua.book@gmail.com>
2016-01-25 18:45:55 -0500

    add boxes and styles for rudimentary layout

--- app/app.js
+++ app/app.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './stylesheets/normalize.css';
+import './stylesheets/main.scss';
+
 var App = React.createClass({
   render () {
     return (
-      <div id='header'>
-        hello world
-      </div>
-      <div id='pieces'>
-        hello world
-      </div>
-      <div id='bio-and-links'>
-        <div id='bio'>
+      <div>
+        <div id='header' className='header'>
+          BOOVIUS PROJECTS
         </div>
-        <div id='links'>
+        <div id='main' className='pad-sides'>
+          <div id='pieces' className='layout pad-sides'>
+            Pieces
+          </div>
+          <div id='bio-and-links' className='layout pad-sides'>
+            <div id='bio'>bio</div>
+            <div id='links'/>
+          </div>
         </div>
       </div>
     )
