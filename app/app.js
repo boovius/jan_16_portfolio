e9cf0a8dd6673dec29c9fc3c9f82aa89ed0a3b05
Joshua Book <joshua.book@gmail.com>
2016-01-25 18:11:23 -0500

    skeleton for app

--- app/app.js
+++ app/app.js
@@ -4,8 +4,17 @@ import ReactDOM from 'react-dom';
 var App = React.createClass({
   render () {
     return (
-      <div>
+      <div id='header'>
         hello world
+      </div>
+      <div id='pieces'>
+        hello world
+      </div>
+      <div id='bio-and-links'>
+        <div id='bio'>
+        </div>
+        <div id='links'>
+        </div>
       </div>
     )
   }
