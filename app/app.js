30b47afa74324405f3647e09305f51b397fc6fdb
Joshua Book <joshua.book@gmail.com>
2016-01-25 17:33:13 -0500

    add basic app entry points for js and html

---
+++ app/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = React.createClass({
+  render () {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('container')
+);
+
