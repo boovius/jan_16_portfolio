98754a94303e36925b175505e33046fecdadb067
Joshua Book <joshua.book@gmail.com>
2016-01-26 18:10:34 -0500

    add merge, fix image styling, and base image paths on name

--- app/piece.js
+++ app/piece.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import merge from 'merge';
 
 import './stylesheets/piece.scss';
 
-let backgroundImage = require('./images/streetbeat.png');
-let image = {backgroundImage: 'url(' + backgroundImage + ')'}
-
 export default class Piece extends React.Component {
   render () {
+    let backgroundImage = require('./images/' + this.props.piece.name.toLowerCase()  + '.png');
+    let image = {backgroundImage: 'url(' + backgroundImage + ')'}
+    let imageStyles = merge(image, {
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    });
+
     return (
       <div className='piece'>
-        <div style={image} className='piece-thumbnail'>
-        </div>
+        <div style={imageStyles} className='piece-thumbnail'/>
         <div className='piece-title'>
           <h1>{this.props.piece.name}</h1>
         </div>
