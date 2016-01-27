0f782a56fe44a17b4232ef140d4443727b9430e5
Joshua Book <joshua.book@gmail.com>
2016-01-26 20:52:22 -0500

    add pieces details to data

--- app/app.js
+++ app/app.js
@@ -7,10 +7,38 @@ import './stylesheets/normalize.css';
 import './stylesheets/main.scss';
 
 const PiecesData = [
-  {name: 'StreetBeat', caption: 'the coolest app ever'},
-  {name: 'Enrichual', caption: 'the coolest app ever'},
-  {name: 'Doo2Nite', caption: 'the coolest app ever'},
-  {name: 'Shavasana', caption: 'the coolest app ever'},
+  {
+    name: 'StreetBeat',
+    caption: {
+      summary: 'Find live music events happening right now, right around you.',
+      buildDetails: 'Built in Rails 4 and Postgresql, utilizing LeafletJS maps engine, Geocoder location services, and Eventful API.',
+    },
+    url: 'http://streetbeat.herokuapp.com/',
+  },
+  {
+    name: 'Enrichual',
+    caption: {
+      summary: 'Find and implement simple rituals into your lifestyle to rhythmically enrich yourself.',
+      buildDetails: 'Built in Rails 4 and MongoDB, utilizing clockwork scheduler and sidekiq and twilio API for telephone integration.',
+    },
+    url: 'http://www.enrichual.com/',
+  },
+  {
+    name: 'Doo2Nite',
+    caption: {
+      summary: 'Realtime brainstorming and consensus building. Spend less time figuring out what to do and more time doing it.',
+      buildDetails: 'Built with AngularJS front-end and Firebase backend.  Designed to be used on mobile devices.',
+    },
+    url: 'http://boovius.github.io/doo2nite/#/new',
+  },
+  {
+    name: 'Shavasana',
+    caption: {
+      summary: 'Simply track the activities you want to do on a regular weekly/monthly basis. ',
+      buildDetails: 'Built with AngularJS frontend and Ruby-Sinatra backend, featuring Facebook OAuth login.',
+    },
+    url: 'http://shavasana-server.herokuapp.com/',
+  }
 ]
 
 let pieces = PiecesData.map((piece, i) => {
